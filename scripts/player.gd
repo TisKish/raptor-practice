39331e63e7d2aabc3424ca76d3774d0187ec3a2d
TisKish <kootawsyang@yahoo.com>
2025-05-02 19:17:54 -0500

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBody2D
+
+@export var gravity = 1600
+@export var jump_power = 600
+
+# Reference other nodes within the scene object. Pre-loads them
+@onready var sprite = $AnimatedSprite2D
+@onready var jump_sound = $JumpSound
+@onready var camera = $"/root/World/Camera2D"
+
+var active = true
+var jumps_remaining = 2
+var was_jumping = false
+var jump_pitch = 1.0
+
+func _ready():
+	print("hello world")
+
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	
+	# Active in this case is checking whether the player is still alive
+	if active:
+		# Update the camera position
+		camera.position = position
+		# Reset the player after landing
+		if was_jumping and is_on_floor():
+			was_jumping = false
+			jumps_remaining = 2
+			sprite.play("run")
+			jump_pitch = 1.0
+			
+		# Handle jumping here
+		if Input.is_action_just_pressed("jump") and jumps_remaining > 0:
+			jumps_remaining -= 1
+			was_jumping = true
+			velocity.y = -jump_power
+			
+			if jumps_remaining == 1:
+				sprite.play("jump")
+			else: 
+				sprite.play("double jump")
+				
+			jump_sound.set_pitch_scale(jump_pitch)
+			jump_sound.play()
+			jump_pitch += 0.2
+				
+				
+	move_and_slide()
